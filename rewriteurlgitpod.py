5c9267f98f23adfed4e4bdefb709d9a1ddaf08a8
Pavel Turovski <pavelturovski@outlook.com>
2021-05-26 03:44:14 +0000

    pass url through config file instead of environment var

--- rewriteurlgitpod.py
+++ rewriteurlgitpod.py
@@ -1,5 +1,5 @@
 import os
-import subprocess
+import re
 
 workspaceURL = (os.environ['GITPOD_WORKSPACE_URL'])
 strippedURL = (workspaceURL.replace('https://',''))
@@ -7,6 +7,27 @@ strippedURL = (workspaceURL.replace('https://',''))
 webpack = ("https://8090-" + strippedURL)
 baked = ("https://3030-" + strippedURL)
 
-os.system('cd ~')
-os.system("export " "WEBPACK_DEV_URL=\"" + webpack + "\"")
-os.system("export " "BAKED_BASE_URL=\"" + baked + "\"")
+
+#os.system("export " "WEBPACK_DEV_URL=\"" + webpack + "\"")
+#os.system("export " "BAKED_BASE_URL=\"" + baked + "\"")
+
+file = open('.env', 'r')
+filelist = file.readlines()
+file.close()
+found = False
+for line in filelist:
+    if str("WEBPACK_DEV_URL") in line:
+        print ("Found it, not modifying file")
+        found = True
+
+if not found:
+    with open('.env', 'a') as file1:
+        file1.write("\nWEBPACK_DEV_URL=" + webpack)
+        file1.write("\nBAKED_BASE_URL=" + baked)
+        print ("appended WEBPACK_DEV_URL and BAKED_BASE_URL to .env file ")
+
+
+
+
+
+
